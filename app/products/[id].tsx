a6f3fba43aa2bfd6011b4721a7a7eda71870428a
Daniela Di Donato Vassallo <didonatodani@gmail.com>
2025-02-06 15:55:02 +0100

    toggling fav star working, and saving that data in async storage

--- app/products/[id].tsx
+++ app/products/[id].tsx
@@ -1,14 +1,17 @@
 import { Product } from '@/types/interfaces'
-import { useLocalSearchParams } from 'expo-router'
+import { useLocalSearchParams, Stack } from 'expo-router' 
 import { useEffect, useState } from 'react'
-import { ActivityIndicator, StyleSheet, Text, View, Image } from 'react-native'
+import { ActivityIndicator, StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
 import { COLORS } from '@/constants/colors'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { Ionicons } from '@expo/vector-icons'
 
-
+const FAVORITES_KEY = 'favorites'
 export default function ProductDetails() {
   const { id } = useLocalSearchParams()
   const [product, setProduct] = useState<Product | null>(null)
   const [loading, setLoading] = useState(true)
+  const [isFavorite, setIsFavorite] = useState(false)
 
   useEffect(() => {
     async function fetchProduct() {
@@ -16,12 +19,13 @@ export default function ProductDetails() {
         const response = await fetch(`https://fakestoreapi.com/products/${id}`)
         const data = await response.json()
         setProduct(data)
-        setLoading(false)
+        checkFavoriteStatus()
       } catch (error) {
         console.error('Error fetching product:', error)
+      } finally {
+        setLoading(false)
       }
     }
-
     fetchProduct()
   }, [id])
 
@@ -29,20 +33,60 @@ export default function ProductDetails() {
     return <ActivityIndicator size='large' color={COLORS.text} />
   }
 
+  async function checkFavoriteStatus() {
+    try {
+      const favorites = await AsyncStorage.getItem(FAVORITES_KEY)
+
+
+      if (favorites) {
+        const favoriteProducts = JSON.parse(favorites) as Product[];
+        setIsFavorite(favoriteProducts.some(product => product.id.toString() === id))
+      }
+    } catch (error) {
+      console.error('Error checking favorite status:', error)
+    }
+  }
+
+  async function toggleFavorite() {
+    try {
+      const favorites = await AsyncStorage.getItem(FAVORITES_KEY) 
+      let favoriteProducts = favorites ? JSON.parse(favorites) as Product[] : []
+
+
+      if(isFavorite){
+        favoriteProducts = favoriteProducts.filter(product => product.id.toString() !== id)
+      } else {
+        favoriteProducts.push(product as Product)
+      }
+
+      await AsyncStorage.setItem(FAVORITES_KEY, JSON.stringify(favoriteProducts))
+      setIsFavorite(!isFavorite)
+
+    } catch (error) {
+      console.error('Error toggling favorite:', error)
+    }
+
+  } 
+
   return (
     <View style={styles.container}>
+      <Stack.Screen options={{
+        headerRight: () => (
+          <TouchableOpacity onPress={toggleFavorite}>
+            <Ionicons name={isFavorite ? 'heart' : 'heart-outline'} size={24} color={COLORS.text} />
+          </TouchableOpacity>
+        )
+      }}/>
       <Image source={{ uri: product?.image }} style={styles.image} />
       <View style={styles.productDetailsContainer}>
+
         <Text style={styles.productTitle}>{product?.title}</Text>
         <Text style={styles.productPrice}>{product?.price} €</Text>
         <Text style={styles.productCategory}>{product?.category.toUpperCase()}</Text>
         <Text style={styles.productDescription}>{product?.description}</Text>
-
-
       </View>
     </View>
   )
-
 }
 
 const styles = StyleSheet.create({
